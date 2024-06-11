35e2703397a342f75e98b9595397167fe7d3f479
Rahul Kumar <92990906+Rahulkumarhavit@users.noreply.github.com>
2024-06-11 16:19:34 +0530

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import express, { Request, Response } from 'express';
+import bodyParser from "body-parser";
+import { SQSClient, ReceiveMessageCommand, DeleteMessageCommand, SendMessageCommand } from '@aws-sdk/client-sqs';
+import 'dotenv/config'
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.post('/api/queue', async (req: Request, res: Response) => {
+    try {
+        const sqsClient = new SQSClient({
+            region: process.env.MY_AWS_REGION,
+        });
+
+        const command = new SendMessageCommand({
+            QueueUrl: process.env.ORDER_QUEUE_URL,
+            MessageBody: JSON.stringify(req.body),
+        });
+
+        await sqsClient.send(command);
+
+        res.send({ message: 'Message sent to the queue' });
+    } catch (error) {
+        res.status(500).send({ message: 'Failed to send message to the queue', error: error });
+    }
+});
+
+app.get('/api/recieve', async (req: Request, res: Response) => {
+    const sqs = new SQSClient({ region: process.env.MY_AWS_REGION });
+    const queueUrl = process.env.ORDER_QUEUE_URL;
+    const params = {
+        QueueUrl: queueUrl,
+        MaxNumberOfMessages: 10, // Number of messages to fetch
+        WaitTimeSeconds: 20, // Long polling
+        MessageAttributeNames: ["All"],
+        VisibilityTimeout: 20,
+    };
+
+    try {
+        const data = await sqs.send(new ReceiveMessageCommand(params));
+
+        if (data.Messages && data.Messages.length > 0) {
+            const messageIds: string[] = [];
+
+            for (const message of data.Messages) {
+                console.log("Message received: ", message.Body);
+
+                // Process the message here (e.g., perform some business logic)
+
+                // Delete the message after processing
+                const deleteParams = {
+                    QueueUrl: queueUrl,
+                    ReceiptHandle: message.ReceiptHandle!,
+                };
+
+                await sqs.send(new DeleteMessageCommand(deleteParams));
+                messageIds.push(message.MessageId!);
+            }
+
+            res.send({ message: 'Messages processed and deleted', messageIds: messageIds });
+        } else {
+            res.send({ message: "No messages to process" });
+        }
+    } catch (error) {
+        console.error("Error receiving messages: ", error);
+        res.status(500).send({ message: "Error receiving messages", error: error });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
